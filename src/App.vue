3f562615bafce7f8ad01491e4db037edc71cab4c
notwizzard <im.morgun.ivan@gmail.com>
2026-06-06 19:00:17 +0600

    💄 Add app shell with top bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
-//
+import UsersView from './views/UsersView.vue'
 </script>
 
 <template>
-  <div>User CRUD</div>
+  <v-app>
+    <v-app-bar flat color="surface" border>
+      <v-app-bar-title class="font-weight-bold">
+        <v-icon icon="mdi-account-group" color="primary" class="mr-2" />
+        Пользователи
+      </v-app-bar-title>
+    </v-app-bar>
+
+    <v-main class="bg-background">
+      <UsersView />
+    </v-main>
+  </v-app>
 </template>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useUsersStore } from '../stores/users'
+
+const store = useUsersStore()
+</script>
+
+<template>
+  <v-container class="py-8" style="max-width: 1100px">
+    <div class="d-flex align-center justify-space-between mb-6">
+      <div>
+        <h1 class="text-h5 font-weight-bold">Список пользователей</h1>
+        <p class="text-medium-emphasis mb-0">Всего: {{ store.total }}</p>
+      </div>
+    </div>
+  </v-container>
+</template>
