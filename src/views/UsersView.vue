e550654ea9f8cefad93655335868e4f06e4c2c40
notwizzard <im.morgun.ivan@gmail.com>
2026-06-06 20:03:20 +0600

    ✨ Wire up create and edit actions

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,11 +1,27 @@
 <script setup>
+import { ref } from 'vue'
 import { useUsersStore } from '../stores/users'
 import UserTable from '../components/UserTable.vue'
+import UserFormDialog from '../components/UserFormDialog.vue'
 
 const store = useUsersStore()
 
+const dialog = ref(false)
+const editing = ref(null)
+
+function openCreate() {
+  editing.value = null
+  dialog.value = true
+}
+
 function onEdit(user) {
-  console.log('edit', user)
+  editing.value = user
+  dialog.value = true
+}
+
+function onSave(data) {
+  if (editing.value) store.update(editing.value.id, data)
+  else store.add(data)
 }
 
 function onDelete(user) {
@@ -20,10 +36,15 @@ function onDelete(user) {
         <h1 class="text-h5 font-weight-bold">Список пользователей</h1>
         <p class="text-medium-emphasis mb-0">Всего: {{ store.total }}</p>
       </div>
+      <v-btn color="primary" variant="flat" prepend-icon="mdi-plus" @click="openCreate">
+        Добавить
+      </v-btn>
     </div>
 
     <v-card border flat>
       <UserTable :users="store.filtered" @edit="onEdit" @delete="onDelete" />
     </v-card>
+
+    <UserFormDialog v-model="dialog" :user="editing" @save="onSave" />
   </v-container>
 </template>
