3800857b367df2d12ff1ea51748c7e3c6b0b07ec
notwizzard <im.morgun.ivan@gmail.com>
2026-06-07 09:15:44 +0600

    💄 Add Inter font and base styles

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import './styles/main.css'
 
 createApp(App).use(createPinia()).use(vuetify).mount('#app')
